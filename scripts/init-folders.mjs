17fcc33a619a056445476e896c25438683fd636b
harekyon <harekyon@gmail.com>
2025-04-22 11:55:21 +0900

    gitkeepを出力しないように

--- scripts/init-folders.mjs
+++ scripts/init-folders.mjs
@@ -5,7 +5,7 @@ const dirs = ["./diffTargetNew", "./diffTargetOld", "./exports"];
 
 for (const dir of dirs) {
   fs.ensureDirSync(dir);
-  fs.ensureFileSync(path.join(dir, ".gitkeep"));
+  // fs.ensureFileSync(path.join(dir, ".gitkeep"));
 }
 
 console.log("📁 初期フォルダ作成完了: diffTargetNew, diffTargetOld, exports");
