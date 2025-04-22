12e96381474d6a071cdd76046fd105a5e967889c
harekyon <harekyon@gmail.com>
2025-04-22 11:13:53 +0900

    差分ジェネレータ1

---
+++ scripts/init-folders.mjs
@@ -0,0 +1,11 @@
+import fs from "fs-extra";
+import path from "path";
+
+const dirs = ["./diffTarget/new", "./diffTarget/old", "./exports"];
+
+for (const dir of dirs) {
+  fs.ensureDirSync(dir);
+  fs.ensureFileSync(path.join(dir, ".gitkeep"));
+}
+
+console.log("📁 初期フォルダ作成完了: diffTarget/new, diffTarget/old, exports");
